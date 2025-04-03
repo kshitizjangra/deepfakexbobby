b28f08d96dba885e66282cc74876eaf753fb8574
Kshitiz Jangra <143020031+kshitizjangra@users.noreply.github.com>
2025-04-04 03:30:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -284,4 +284,4 @@ def serve_static(path):
     return send_from_directory('static', path)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=8080)
